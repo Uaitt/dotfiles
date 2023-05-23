ead3fc9c3692373d096a5460f54f606f2f750edf
Lorenzo Zabot <lorenzozabot@gmail.com>
2023-05-23 22:17:57 +0200

    feat(wezterm): add Dark+ color scheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,10 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = {}
 
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- disable ligatures
+config.harfbuzz_features = { 'calt = 0', 'clig = 0', 'liga = 0' }
 config.enable_scroll_bar = true
+config.color_scheme = 'Dark+'
+
 return config
