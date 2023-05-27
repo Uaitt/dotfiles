00012aaf5eedf980d1d97b2799f562a548ca373c
Lorenzo Zabot <lorenzozabot@gmail.com>
2023-05-27 22:04:30 +0200

    feat(wezterm): increase font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,6 @@ local config = {}
 config.harfbuzz_features = { 'calt = 0', 'clig = 0', 'liga = 0' }
 config.enable_scroll_bar = true
 config.color_scheme = 'Dark+'
-config.font_size = 12
+config.font_size = 13
 
 return config
