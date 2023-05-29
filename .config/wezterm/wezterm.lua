5dc9edb4d26f2ef7500d8391e74a7b33942ba4e3
Lorenzo Zabot <lorenzozabot@gmail.com>
2023-05-29 22:33:16 +0200

    feat(wezterm): maximize window on GUI startup

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,9 +2,17 @@ local wezterm = require 'wezterm'
 
 local config = {}
 
-config.harfbuzz_features = { 'calt = 0', 'clig = 0', 'liga = 0' }
-config.enable_scroll_bar = true
+-- Maximize window on GUI startup
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+-- Appearance
 config.color_scheme = 'Dark+'
 config.font_size = 13
+config.harfbuzz_features = { 'calt = 0', 'clig = 0', 'liga = 0' }
+config.enable_scroll_bar = true
+
 
 return config
