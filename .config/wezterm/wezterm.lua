b3757564d45715ebd6c07ad841198c7435d7beb5
Lorenzo Zabot <lorenzozabot@gmail.com>
2023-08-14 13:04:01 +0200

    feat(wezterm): update titlebar appearance

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,13 +11,13 @@ end)
 config.color_scheme = "Everforest Dark"
 
 -- Fonts
-config.font_size = 12
+config.font_size = 14
 config.font = wezterm.font('FiraCode Nerd Font')
 config.harfbuzz_features = { 'calt = 0', 'clig = 0', 'liga = 0' } -- no ligatures
 
 -- Appearance 
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
+config.window_decorations = "RESIZE"
 
 return config
 
